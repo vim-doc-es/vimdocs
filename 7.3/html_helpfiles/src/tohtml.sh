72a6c24615f39695e48931739140b307a212d4b6
Javier Rojas <jerojasro@devnull.li>
2011-09-25 18:05:30 -0500

    añadido código para hacer la creación de HTML desde el repositorio directamente

--- 7.3/html_helpfiles/src/tohtml.sh
+++ 7.3/html_helpfiles/src/tohtml.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+BIN_DIR=$PWD
+ESX_DIR=$PWD/../../helpfiles/traducido/esx/
+HTML_DIR=$PWD/../data/www-vim-doc-es/
+
+mkdir -p $HTML_DIR
+
+cd $ESX_DIR
 # generate tags file
 vim -c 'helptags ./' -c 'q'
 
@@ -12,4 +19,13 @@ for file in *.esx; do
 done
 
 # add links and references in HTML files
-python add_tags.py *.html
+python $BIN_DIR/add_tags.py *.html
+
+# add search form, and fix title, in each html document
+for file in *.esx; do
+    hfile=$(basename $file .esx).html
+    python $BIN_DIR/html_posproc.py $file $hfile
+done
+
+rm $HTML_DIR/*.html
+mv *.html $HTML_DIR
